a1ca75825533465d08fc7326fa8b0505e072672b
arfelipe <arthurfelipefer@hotmail.com>
2025-10-01 21:55:01 -0300

    3. Classe TestResult

--- test_framework.py
+++ test_framework.py
@@ -1,13 +1,46 @@
+# test_framework.py
+
+class TestResult:
+
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+
+    def test_started(self):
+        self.run_count += 1
+
+    def add_failure(self, test):
+        self.failures.append(test)
+
+    def add_error(self, test):
+        self.errors.append(test)
+
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}'
+
 class TestCase:
 
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
-        self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
-        self.tear_down() # chama método de teardown 
+    def run(self, result):
+        result.test_started()
+        self.set_up()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError:
+            result.add_failure(self.test_method_name)
+        except Exception:
+            result.add_error(self.test_method_name)
+        self.tear_down()
 
     def set_up(self):
         pass
@@ -31,11 +64,27 @@ class MyTest(TestCase):
 
     def test_c(self):
         print('test_c')
+    
+    def test_fails(self):
+        print('test_fails')
+        assert False
+
+    def test_error(self):
+        print('test_error')
+        raise Exception
 
 if __name__ == '__main__':
+    result = TestResult()
     test = MyTest('test_a')
-    test.run()
+    test.run(result)
     test = MyTest('test_b')
-    test.run()
+    test.run(result)
     test = MyTest('test_c')
-    test.run()
+    test.run(result)
+    test = MyTest('test_fails')
+    test.run(result)
+    test = MyTest('test_error')
+    test.run(result)
+
+    print('\n--- SUMMARY ---')
+    print(result.summary())
